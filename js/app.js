019275dc41bd220f8f8d334329f1a851fb64c541
Mel <meljohnpualon@gmail.com>
2021-08-28 14:48:18 +0800

    added active class on active card content

--- js/app.js
+++ js/app.js
@@ -17,5 +17,14 @@ tabBtnContainer.addEventListener('click', e => {
         btn.classList.remove('tab-active')
     })
 
+    // add the class active on clicked target btn
     clicked.classList.add('tab-active')
+
+    // remove the active class on not active content
+    tabContent.forEach(tab => {
+        tab.classList.remove('items-content-active');
+    })
+
+    // now we have to put an active class on a content based on data set items from clicked btn
+    document.querySelector(`.items-content-${clicked.dataset.items}`).classList.add('items-content-active')
 })
